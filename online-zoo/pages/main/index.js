7b0600b5fd66b4370bf852f5b6382bc0314807f6
DzmitrySm <magicjoker@yandex.ru>
2022-10-09 15:51:35 +0300

    feat: add js burger-menu to main and donate page

--- online-zoo/pages/main/index.js
+++ online-zoo/pages/main/index.js
@@ -1,1 +1,32 @@
-alert('Проверьте, пожалуйста, в среду')
+(function () {
+  const burgerMenu = document.querySelector('.burger-menu')  ;
+  const navItems = document.querySelector('.navigation')
+  const NavItems = document.querySelector('.nav-list')
+  const closeMenuByLink = document.querySelectorAll('.nav-link')
+  const designeVisible = document.querySelector('.text-design-burger')
+  const scrollBehaviour = document.querySelector('.body')
+  const logoVisible = document.querySelector('.logo-burger-menu')
+  const overlay = document.querySelector('.overlay-blackout')
+  burgerMenu.addEventListener('click', () => {
+      navItems.classList.toggle('navigation-active')
+      burgerMenu.classList.toggle('burger-menu-active')
+      overlay.classList.toggle('overlay-blackout-active')
+      designeVisible.classList.add('text-design-burger-active')
+      logoVisible.classList.add('logo-burger-menu-active')
+      let paddingOfset = window.innerWidth - document.body.offsetWidth + 'px'
+      document.body.style.paddingRight = paddingOfset
+      scrollBehaviour.classList.toggle('overlay')
+
+  });
+  overlay.addEventListener('click', () => {
+    overlay.classList.remove('overlay-blackout-active')
+    burgerMenu.classList.remove('burger-menu-active')
+    navItems.classList.remove('navigation-active')
+    document.body.style.paddingRight = 0;
+    scrollBehaviour.classList.remove('overlay')
+    designeVisible.classList.remove('text-design-burger-active')
+    logoVisible.classList.remove('logo-burger-menu-active')
+
+  } )
+
+}());
