85c0f2ca626ef805f4de577c46dde76830e25bcc
DzmitrySm <magicjoker@yandex.ru>
2022-10-12 02:14:34 +0300

    feat: add some functions for amount donate panel

--- online-zoo/pages/donate/index2.js
+++ online-zoo/pages/donate/index2.js
@@ -31,3 +31,38 @@
 
 }());
 
+
+
+//Amount
+
+const allPrices = document.querySelectorAll('.price-label')
+const inputNumber = document.querySelector('.input-number')
+const radioButtons = document.querySelectorAll('.nav-button')
+const yellowButtons = document.querySelectorAll('.button-slider')
+const initialPrice = document.querySelector('.btn6')
+
+radioButtons.forEach(x => x.addEventListener('click', (event) => {
+  initialPrice.classList.remove('btn6')
+  yellowButtons.forEach(y => y.classList.remove('circle'))
+event.target.classList.add('circle')
+}))
+
+
+
+
+
+
+  inputNumber.addEventListener('input', () => {
+    allPrices.forEach(price => {
+      if(inputNumber.value > 9999) {
+        inputNumber.value = 9999
+      }
+      if(inputNumber.value < 0) {
+        inputNumber.value = 0
+      }
+      if (price.textContent === inputNumber.value) {
+        price.classList.add('orange-label')
+      }else {
+        price.classList.remove('orange-label')
+      }
+  })})
