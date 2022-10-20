73d6701c31edb7ac084980e16ab3a4fd93d07d4c
DzmitrySm <magicjoker@yandex.ru>
2022-10-21 00:55:40 +0300

    feat: add eslint to project, add play field and shufled numbers into items of play field

--- Codejam-Gem-Puzzle/src/index.js
+++ Codejam-Gem-Puzzle/src/index.js
@@ -1,2 +1,29 @@
-let a=1;
-let b =5
+const body = document.querySelector('body');
+const container = document.createElement('div');
+container.className = 'wrapper';
+body.append(container);
+const playArea = document.createElement('div');
+playArea.className = 'wrapper-play-area';
+container.append(playArea);
+
+function addItems() {
+  for (let i = 0; i < 16; i++) {
+    const item = document.createElement('div');
+    item.className = 'puzzle-item';
+    playArea.append(item);
+  }
+}
+
+addItems();
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let shufleArr = arr.sort(() => Math.random() - 0.5)
+
+function shufleNumbers() {
+  let item = document.querySelectorAll('.puzzle-item')
+  for (let i = 0; i < shufleArr.length; i++) {
+    item[i].innerHTML = shufleArr[i];
+  }
+}
+shufleNumbers()
