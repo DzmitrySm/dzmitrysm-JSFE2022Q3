71e3c51e98f19cb3d8ec22914a30f88c3455315f
DzmitrySm <magicjoker@yandex.ru>
2022-11-15 01:55:32 +0300

    feat: add basic page with game, add custom player, add burger-menu to start page

---
+++ Songbird/src/pages/quizPage/index.js
@@ -0,0 +1,30 @@
+const audio = document.querySelector('.audio-pl');
+const btnPlay = document.querySelector('.circle-btn-play');
+const imgPlay = document.querySelector('.btn-play-img');
+btnPlay.addEventListener('click', () => {
+  imgPlay.src = '../../assets/images/pause-btn.png';
+  audio.src = '../../assets/sounds/tit-bird.mp3';
+  playAudio()
+})
+
+
+let isPlay = false;
+function playAudio() {
+  if(!isPlay) {
+    audio.currentTime = 0;
+    audio.play();
+    isPlay = true;
+  }
+  else if (isPlay) {
+    audio.pause();
+    isPlay = false;
+    imgPlay.src = '../../assets/images/play-btn.png';
+  }
+}
+
+
+/*function currentDuration(time) {
+  let min = Math.floor(time % 60);
+  let sec =  
+}*/
+
