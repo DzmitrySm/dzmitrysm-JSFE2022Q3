091d645a89fc038e74e32bdf67789d251a94f5c7
DzmitrySm <magicjoker@yandex.ru>
2022-11-15 22:54:16 +0300

    feat: add adaptive to start page, add js for burger-menu and overlay

--- Songbird/src/pages/quizPage/index.js
+++ Songbird/src/pages/quizPage/index.js
@@ -4,27 +4,23 @@ const imgPlay = document.querySelector('.btn-play-img');
 btnPlay.addEventListener('click', () => {
   imgPlay.src = '../../assets/images/pause-btn.png';
   audio.src = '../../assets/sounds/tit-bird.mp3';
-  playAudio()
-})
-
+  playAudio();
+});
 
 let isPlay = false;
 function playAudio() {
-  if(!isPlay) {
+  if (!isPlay) {
     audio.currentTime = 0;
     audio.play();
     isPlay = true;
-  }
-  else if (isPlay) {
+  } else if (isPlay) {
     audio.pause();
     isPlay = false;
     imgPlay.src = '../../assets/images/play-btn.png';
   }
 }
 
-
-/*function currentDuration(time) {
+/* function currentDuration(time) {
   let min = Math.floor(time % 60);
-  let sec =  
-}*/
-
+  let sec =
+} */
