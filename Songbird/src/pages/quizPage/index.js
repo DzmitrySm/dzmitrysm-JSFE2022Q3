ea29580d105c7280a57159f1225815e975b31ef3
DzmitrySm <magicjoker@yandex.ru>
2022-11-16 00:39:24 +0300

    feat: add function currentDuration

--- Songbird/src/pages/quizPage/index.js
+++ Songbird/src/pages/quizPage/index.js
@@ -1,6 +1,7 @@
 const audio = document.querySelector('.audio-pl');
 const btnPlay = document.querySelector('.circle-btn-play');
 const imgPlay = document.querySelector('.btn-play-img');
+const currSongDur = document.querySelector('.current-song-duration');
 btnPlay.addEventListener('click', () => {
   imgPlay.src = '../../assets/images/pause-btn.png';
   audio.src = '../../assets/sounds/tit-bird.mp3';
@@ -20,7 +21,21 @@ function playAudio() {
   }
 }
 
-/* function currentDuration(time) {
+audio.addEventListener('timeupdate', function () {
+  currSongDur.innerHTML = currentDuration(audio.currentTime);
+}, false);
+
+function currentDuration(time) {
   let min = Math.floor(time % 60);
-  let sec =
-} */
+  min = 0;
+  let sec = Math.ceil((time / 60 * 60) % 60)
+ if(sec === 60) {
+  sec = 0;
+  min = min + 1;
+}
+if(sec < 10) {
+  sec = `0${sec}`
+}
+return `${min}:${sec}`
+
+}
