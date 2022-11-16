af96f744e11fd258342e90294d2b369596dc4a22
DzmitrySm <magicjoker@yandex.ru>
2022-11-16 22:09:42 +0300

    feat: add function for volume input

--- Songbird/src/pages/quizPage/index.js
+++ Songbird/src/pages/quizPage/index.js
@@ -2,6 +2,8 @@ const audio = document.querySelector('.audio-pl');
 const btnPlay = document.querySelector('.circle-btn-play');
 const imgPlay = document.querySelector('.btn-play-img');
 const currSongDur = document.querySelector('.current-song-duration');
+const volumeInput = document.querySelector('.inp-vol-range');
+const volumeImg = document.querySelector('.volume-icon');
 btnPlay.addEventListener('click', () => {
   imgPlay.src = '../../assets/images/pause-btn.png';
   audio.src = '../../assets/sounds/tit-bird.mp3';
@@ -39,3 +41,18 @@ if(sec < 10) {
 return `${min}:${sec}`
 
 }
+
+
+volumeInput.addEventListener('input', () => {
+  audio.volume = volumeInput.value;
+  if (volumeInput.value === '0') {
+    volumeImg.src = '../../assets/images/volume-off.png';
+  }else {
+    volumeImg.src = '../../assets/images/volume-on.png';
+  }
+}, false);
+
+audio.addEventListener('ended', () => {
+  audio.currentTime = 0;
+  imgPlay.src = '../../assets/images/play-btn.png';
+}, false);
