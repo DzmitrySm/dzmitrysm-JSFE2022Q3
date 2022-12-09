cf558e89326ff816b045b7c000fad0ad5d285b3c
DzmitrySm <magicjoker@yandex.ru>
2022-12-09 22:31:31 +0300

    feat: type loader.ts

---
+++ Migration to TypeScript/src/components/controller/loader.ts
@@ -0,0 +1,50 @@
+import { Iloader } from '../../types/index';
+import { Callback } from '../../types/index';
+class Loader implements Iloader {
+    baseLink: string;
+    options: { apiKey: string };
+    constructor(baseLink: string, options: { apiKey: string }) {
+        this.baseLink = baseLink;
+        this.options = options;
+    }
+
+    getResp<T>(
+        { endpoint = '', options = {} },
+        callback: Callback<T> = (): void => {
+            console.error('No callback for GET response');
+        }
+    ) {
+        this.load('GET', endpoint, callback, options);
+    }
+
+    errorHandler(res: Response) {
+        if (!res.ok) {
+            if (res.status === 401 || res.status === 404)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+
+        return res;
+    }
+
+    makeUrl(options = {}, endpoint: string) {
+        const urlOptions: { [index: string]: string } = { ...this.options, ...options };
+        let url = `${this.baseLink}${endpoint}?`;
+
+        Object.keys(urlOptions).forEach((key) => {
+            url += `${key}=${urlOptions[key]}&`;
+        });
+
+        return url.slice(0, -1);
+    }
+
+    load<T>(method: string, endpoint: string, callback: Callback<T>, options = {}): void {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data) => callback(data))
+            .catch((err) => console.error(err));
+    }
+}
+
+export default Loader;
