21f862c1e078ca517895492f6c2e4dce6018c734
DzmitrySm <magicjoker@yandex.ru>
2022-12-10 13:28:24 +0300

    feat: type controller.ts

---
+++ Migration to TypeScript/src/components/controller/controller.ts
@@ -0,0 +1,44 @@
+import AppLoader from './appLoader';
+import { INews } from '../../types/index';
+import { ISources } from '../../types/index';
+import { Callback } from '../../types/index';
+
+class AppController extends AppLoader {
+    getSources(callback: Callback<ISources>) {
+        super.getResp(
+            {
+                endpoint: 'sources',
+            },
+            callback
+        );
+    }
+
+    getNews(e: Event, callback: Callback<INews>) {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
+
+        while (target !== newsContainer) {
+            if (target.classList.contains('source__item')) {
+                const sourceId = target.getAttribute('data-source-id') as string;
+                if (newsContainer.getAttribute('data-source') !== sourceId) {
+                    newsContainer.setAttribute('data-source', sourceId);
+                    super.getResp(
+                        {
+                            endpoint: 'everything',
+                            options: {
+                                sources: sourceId,
+                            },
+                        },
+                        callback
+                    );
+                }
+                return;
+            }
+            if (target !== null) {
+                target = target.parentNode as HTMLElement;
+            }
+        }
+    }
+}
+
+export default AppController;
