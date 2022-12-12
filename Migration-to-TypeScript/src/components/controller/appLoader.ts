7f03a3b2cd1624be45bc4d8701c8cdb5379b5d50
DzmitrySm <magicjoker@yandex.ru>
2022-12-13 00:07:25 +0300

    fix: change link for deploy

--- Migration-to-TypeScript/src/components/controller/appLoader.ts
+++ Migration-to-TypeScript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
             apiKey: 'd836b3f09ad24676867dd02f7ec5b7ba', // получите свой ключ https://newsapi.org/
         });
     }
