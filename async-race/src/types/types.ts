99a4e0ca6476989c5a9a46034b007162ed5cb59a
DzmitrySm <magicjoker@yandex.ru>
2023-01-21 01:02:54 +0300

    feat: add interfaces for car and server responce

--- async-race/src/types/types.ts
+++ async-race/src/types/types.ts
@@ -1,12 +1,12 @@
 export interface ICar {
   name: string;
   color: string;
-  id?: number | string;
+  id?: number;
 }
 
 export type ICars = ICar[];
 
 export interface IRespCars {
-  items: ICars;
-  count?: number | string;
+  cars: ICars;
+  number?: number | string;
 }
